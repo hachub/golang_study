b1e486b6afa53e8327c8b99c85b4e3d745e97acc
Alexander Medvedev <amedvedev@axmor.com>
2021-04-03 02:37:29 +0700

    fixed https://www.hackerrank.com/challenges/kangaroo/problem

--- hackerrank/001_number_line_jumps.go
+++ hackerrank/001_number_line_jumps.go
@@ -9,5 +9,14 @@ func kangaroo(x1 int32, v1 int32, x2 int32, v2 int32) string {
 	if v1 > x2-x1+v2 {
 		return "NO"
 	}
+	dx := x2*v1 - x1*v2
+	dv := v1 - v2
+	if dx%dv > 0 {
+		return "NO"
+	}
+	k := dx / dv
+	if (k-x1)%v1 > 0 {
+		return "NO"
+	}
 	return "YES"
 }
